221ea002a9a3c80bb155a7dc6cb67e29457a26b9
nastaj <G00424689@atu.ie>
2023-10-16 13:48:34 +0200

    preview tablet media queries

--- src/features/preview/PreviewNav.jsx
+++ src/features/preview/PreviewNav.jsx
@@ -9,10 +9,10 @@ function PreviewNav() {
   const hasCreated = UrlUserId === user.id;
 
   return (
-    <nav className="mb-16 flex gap-4 p-4">
+    <nav className="mb-16 flex gap-4 p-4 md:justify-between">
       {isAuthenticated && hasCreated && (
         <>
-          <Button variation="secondary" type="link">
+          <Button variation="secondary" type="link" className="md:w-auto">
             Back to Editor
           </Button>
           <Button onClick={UrlToClipboard}>Share Link</Button>
--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -5,11 +5,13 @@ import PreviewNav from "../features/preview/PreviewNav";
 function Preview() {
   return (
     <>
-      <header>
-        <PreviewNav />
-      </header>
-      <main className="flex flex-col items-center justify-center">
-        <div className="flex w-3/5 flex-col items-center justify-center gap-14">
+      <div className="md:h-[35vh] md:rounded-b-[2rem] md:bg-brand-purple md:p-6">
+        <header className="md:rounded-xl md:bg-white">
+          <PreviewNav />
+        </header>
+      </div>
+      <main className="flex flex-col items-center justify-center bg-white md:absolute md:left-1/2 md:top-48 md:w-[35vw] md:-translate-x-1/2 md:rounded-3xl md:px-14 md:py-12 md:drop-shadow-xl">
+        <div className="flex flex-col items-center justify-center gap-14">
           <PreviewDetails />
           <PreviewLinkList />
         </div>
